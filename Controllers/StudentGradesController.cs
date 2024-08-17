d7cbde862cd0dc4d07d332f6b5c80eb60a4f2be2
hishamabdalla <hishvmabdalla@gmail.com>
2024-08-17 16:11:52 +0300

    Day3

---
+++ Controllers/StudentGradesController.cs
@@ -0,0 +1,33 @@
+﻿using ITI.Models;
+using ITI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.ProjectModel;
+using System.Drawing;
+
+namespace ITI.Controllers
+{
+    public class StudentGradesController : Controller
+    {
+        ITIEntity context=new ITIEntity();
+
+
+        public IActionResult Details(int id)
+        {
+            Trainee traineeModel=context.Trainees.FirstOrDefault(t=>t.Id==id);
+            CrsResult crsResultModel=context.CrsResult.FirstOrDefault(t=>t.Trainee_Id==id);
+            Course courseModel=context.Courses.FirstOrDefault(t=>t.Id==id);
+
+            TraineeDetailsViewModel traineeViewModel = new TraineeDetailsViewModel()
+            {
+                TraineeId = traineeModel.Id,
+                TraineeName = traineeModel.Name,
+                CourseName = courseModel.Name,
+                Degree = crsResultModel.Degree,
+                DegreeColor = crsResultModel.Degree >= courseModel.MinDegree ? "Green" : "Red"
+
+            };
+
+            return View(traineeViewModel);
+        }
+    }
+}
---
+++ ViewModel/TraineeDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ITI.ViewModel
+{
+    public class TraineeDetailsViewModel
+    {
+        public int TraineeId {  get; set; }
+        public string TraineeName { get; set; }
+
+        public string CourseName { get; set; }
+
+        public int Degree {  get; set; }
+
+        public string DegreeColor { get; set; }
+    }
+}
